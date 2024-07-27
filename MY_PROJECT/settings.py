f1976ae7f71b5f51b5e286a1d981f1648da98542
davidcalikes <davidcalikes@gmail.com>
2024-07-27 23:30:02 +0000

    task: Truncate gitpod url in allowed hosts

--- MY_PROJECT/settings.py
+++ MY_PROJECT/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-davidcalike-djangosetup-7vmbjsqazwa.ws-eu115.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
